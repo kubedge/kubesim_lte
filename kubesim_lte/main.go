a83081d50216335c895c4053712aea9ad5a0c50d
Robert Boyd <rb422w@att.com>
2018-10-13 20:45:24 +0000

    [XXXXX] import and call client
    Change-Id: I26ffd0a0750ad59d60ffd5ba51d388d5928db11e

--- kubesim_lte/main.go
+++ kubesim_lte/main.go
@@ -1,7 +1,10 @@
 package main
 import (
+  "log"
+  "os"
   "net/http"
   "strings"
+  "github.com/kubedge/kubesim_base/grpc/go/kubedge_client"
 )
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
@@ -9,9 +12,20 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
   message = "Hello " + message
   w.Write([]byte(message))
 }
+
+//arguments
+// arg1=demotype
+// arg2=demovalue
 func main() {
+  log.Printf("%s", "kubesim_lte client is running")
+  demotype := os.Args[1]
+  demovalue := os.Args[2]
+  log.Printf("demotype=%s, demovalue=%s", demotype, demovalue)
+  go client.Client(demotype, demovalue)
+
   http.HandleFunc("/", sayHello)
   if err := http.ListenAndServe(":8080", nil); err != nil {
     panic(err)
   }
+  log.Printf("%s", "kubesim_lte client is exiting")
 }
