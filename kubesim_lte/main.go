b7f5ba14a36c7b5827bdbe597a696f230bc6a1ad
Robert Boyd <rb422w@att.com>
2018-10-15 22:50:22 +0000

    [XXXXX] loop read connected file every 15 seconds and send to server.    Also added read/print of config yaml data.    Modified Dockerfile to pass 2 hardcoded args to client.
    Change-Id: Iefa809ea769430d54ffdd9951b77918ec289ee66

--- kubesim_lte/main.go
+++ kubesim_lte/main.go
@@ -4,7 +4,10 @@ import (
   "os"
   "net/http"
   "strings"
+  "time"
   "github.com/kubedge/kubesim_base/grpc/go/kubedge_client"
+  "github.com/kubedge/kubesim_base/config"
+  "github.com/kubedge/kubesim_base/connected"
 )
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
@@ -21,7 +24,27 @@ func main() {
   demotype := os.Args[1]
   demovalue := os.Args[2]
   log.Printf("demotype=%s, demovalue=%s", demotype, demovalue)
-  go client.Client(demotype, demovalue)
+
+  var conf config.Configdata
+  conf.Config()
+  log.Printf("lte client:  product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
+             conf.Product_name, conf.Product_type, conf.Product_family, conf.Product_release, conf.Feature_set1, conf.Feature_set2)
+
+
+  var conn connected.Connecteddata
+  var message string
+
+  log.Printf("starting for loop")
+  for {
+     conn.Readconnectvalues()
+     log.Printf("epc server:  connected=%s", conn.Connected)
+
+     message = strings.Join(conn.Connected, ",") 
+     log.Printf("message to send=" + message)
+     client.Client(demotype, message)
+
+     time.Sleep(15 * time.Second)  //every 15 seconds
+  }
 
   http.HandleFunc("/", sayHello)
   if err := http.ListenAndServe(":8080", nil); err != nil {
